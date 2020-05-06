0d39bfc2dd390f1217f7fa3dae6e9717b15b5a30
Yanzu-zz <2273359360@qq.com>
2020-05-06 22:02:01 +0800

    add Type definition file

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+// 项目中所有的类型编译文件，即类型定义
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+  headers?: any
+}
